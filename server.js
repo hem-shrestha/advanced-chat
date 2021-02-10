235500dee5f5f0fd8a28cb892c6375e74ad263a4
hem-shrestha <62273037+hem-shrestha@users.noreply.github.com>
2021-02-10 11:09:26 +0545

    port 3000

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require('express')
 , _ = require('underscore')._;
 
 app.configure(function() {
-	app.set('port', process.env.PORT || 8080);
+	app.set('port', process.env.PORT || 3000);
   	app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0");
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
